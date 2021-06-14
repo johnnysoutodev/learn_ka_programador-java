47732d2ca5680c27dbfeb4991336617585c73bf9
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-14 20:52:41 -0300

    Initial commit

---
+++ TreinamentoJava/src/model/ClienteTO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Johnny Souto
+ */
+public class ClienteTO {
+    private int codigo;
+    private String nome;
+
+    public ClienteTO() {
+    }
+
+    public ClienteTO(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
