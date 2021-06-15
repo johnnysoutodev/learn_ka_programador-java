15fca025a3224be67f11c2a73a0b18c25cbd7e85
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-14 21:14:07 -0300

    Create TesteClienteTO.java

---
+++ TreinamentoJava/src/view/TesteClienteTO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import model.ClienteTO;
+
+/**
+ *
+ * @author Johnny Souto
+ */
+public class TesteClienteTO {
+    
+    public static void main(String args[]){
+        ClienteTO objClienteTO = new ClienteTO();
+    }
+    
+}
