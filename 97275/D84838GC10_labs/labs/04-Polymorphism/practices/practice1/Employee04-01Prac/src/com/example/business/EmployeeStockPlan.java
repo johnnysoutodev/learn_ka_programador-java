096a659c96bfa0ccbab3bee8dcec4dd2a5250d3a
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-16 20:47:02 -0300

    Update EmployeeStockPlan.java

--- 97275/D84838GC10_labs/labs/04-Polymorphism/practices/practice1/Employee04-01Prac/src/com/example/business/EmployeeStockPlan.java
+++ 97275/D84838GC10_labs/labs/04-Polymorphism/practices/practice1/Employee04-01Prac/src/com/example/business/EmployeeStockPlan.java
@@ -5,10 +5,32 @@
  */
 package com.example.business;
 
+import com.example.domain.Director;
+import com.example.domain.Employee;
+import com.example.domain.Manager;
+
 /**
  *
  * @author Johnny Souto
  */
 public class EmployeeStockPlan {
+    private final int employeeShares = 10;
+    private final int managerShares = 100;
+    private final int directorShares = 1000;
+
+    public EmployeeStockPlan() {
+    }
+    
+    public int grantStock(Employee emp){
+        if(emp instanceof Director){
+            return directorShares;
+        } else {
+            if(emp instanceof Manager){
+                return managerShares;
+            } else {
+                return employeeShares;
+            }
+        }
+    }
     
 }
