b77087dfc63be9fccc4e154e496ebcebcf3a7946
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-16 21:22:16 -0300

    Create EstadoCivil.java

---
+++ TreinamentoJava/src/enums/EstadoCivil.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enums;
+
+/**
+ *
+ * @author johnny
+ */
+public enum EstadoCivil {
+    CASADO,
+    SOLTEIRO,
+    VIUVO,
+    DESQUITADO,
+    UNIAO_ESTAVEL;
+}
